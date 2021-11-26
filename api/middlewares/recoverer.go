9d1caec6f9f9d4209d16ddc0003e3652cf682bb3
Ivan Stasiuk <brokeyourbike@gmail.com>
2021-11-26 15:53:06 +0200

    fix: simplify recoverer

--- api/middlewares/recoverer.go
+++ api/middlewares/recoverer.go
@@ -4,26 +4,24 @@ import (
 	"net/http"
 	"runtime/debug"
 
-	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 )
 
 // Recoverer is a middleware that recovers from panics, logs the panic (and a
 // backtrace), and returns a HTTP 500 (Internal Server Error) status if possible.
-func Recoverer(logger *logrus.Logger) func(next http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		fn := func(w http.ResponseWriter, r *http.Request) {
-			defer func() {
-				if rvr := recover(); rvr != nil && rvr != http.ErrAbortHandler {
-					logger.WithFields(logrus.Fields{
-						"stacktrace": string(debug.Stack()),
-					}).Warnf("%+v", rvr)
+func Recoverer(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if rvr := recover(); rvr != nil && rvr != http.ErrAbortHandler {
+				log.WithFields(log.Fields{
+					"stacktrace": string(debug.Stack()),
+				}).Fatalf("%+v", rvr)
 
-					w.WriteHeader(http.StatusInternalServerError)
-				}
-			}()
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+		}()
 
-			next.ServeHTTP(w, r)
-		}
-		return http.HandlerFunc(fn)
+		next.ServeHTTP(w, r)
 	}
+	return http.HandlerFunc(fn)
 }
