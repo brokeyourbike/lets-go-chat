b4b779b6d4dc4b1b45ab9303faebff99ba2d83a6
Ivan Stasiuk <brokeyourbike@gmail.com>
2021-12-07 17:32:45 +0200

    test: use `testify`

--- pkg/hasher/hasher_test.go
+++ pkg/hasher/hasher_test.go
@@ -1,16 +1,22 @@
 package hasher
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestHashPassword(t *testing.T) {
-	if _, err := HashPassword("super-secret-password"); err != nil {
-		t.Errorf("TestHashPassword() = %q, want %v", err, nil)
-	}
+	password := "super-secret-password"
+	hashed, err := HashPassword(password)
+
+	require.NoError(t, err)
+	require.NotEqual(t, password, hashed)
 }
 
 func TestCheckPasswordHash(t *testing.T) {
-	want := "$2a$04$09IzQ3oawFacAKHjG7QFneYFIaxV2fCNy7RG63RlFKQd.1ChHU6Xa"
-	if got := CheckPasswordHash("super-secret-password", want); got == false {
-		t.Errorf("TestCheckPasswordHash() = %v, want %q", got, want)
-	}
+	hash := "$2a$04$09IzQ3oawFacAKHjG7QFneYFIaxV2fCNy7RG63RlFKQd.1ChHU6Xa"
+	isValid := CheckPasswordHash("super-secret-password", hash)
+
+	require.True(t, isValid)
 }
