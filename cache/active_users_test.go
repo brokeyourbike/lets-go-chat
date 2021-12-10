117d273f6ecc0fea7bd6895944e0de5ecbe03232
Ivan Stasiuk <brokeyourbike@gmail.com>
2021-12-10 13:34:27 +0200

    test: more cases for `active_users`

--- cache/active_users_test.go
+++ cache/active_users_test.go
@@ -9,32 +9,108 @@ import (
 )
 
 func TestAdd(t *testing.T) {
-	repo := NewActiveUsersRepo()
-	key := uuid.New()
+	cases := map[string]struct {
+		setup     func(t *testing.T, repo *ActiveUsersRepo)
+		wantCount int
+	}{
+		"it can Add item": {
+			setup: func(t *testing.T, repo *ActiveUsersRepo) {
+				key := uuid.New()
+				require.NoError(t, repo.Add(key))
+			},
+			wantCount: 1,
+		},
+		"Add two items with the same key": {
+			setup: func(t *testing.T, repo *ActiveUsersRepo) {
+				key := uuid.New()
+				require.NoError(t, repo.Add(key))
+				require.NoError(t, repo.Add(key))
+			},
+			wantCount: 1,
+		},
+		"Add two items with different keys": {
+			setup: func(t *testing.T, repo *ActiveUsersRepo) {
+				key1 := uuid.New()
+				key2 := uuid.New()
 
-	err := repo.Add(key)
-	require.NoError(t, err)
+				require.NoError(t, repo.Add(key1))
+				require.NoError(t, repo.Add(key2))
+			},
+			wantCount: 2,
+		},
+	}
 
-	assert.Equal(t, 1, repo.Count())
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			repo := NewActiveUsersRepo()
+			c.setup(t, repo)
+
+			assert.Equal(t, c.wantCount, repo.Count())
+		})
+	}
 }
 
 func TestDelete(t *testing.T) {
-	repo := NewActiveUsersRepo()
-	key := uuid.New()
+	cases := map[string]struct {
+		setup     func(t *testing.T, repo *ActiveUsersRepo)
+		wantCount int
+	}{
+		"Delete can remove item": {
+			setup: func(t *testing.T, repo *ActiveUsersRepo) {
+				key := uuid.New()
 
-	require.NoError(t, repo.Add(key))
-	require.Equal(t, 1, repo.Count())
+				require.NoError(t, repo.Add(key))
+				repo.Delete(key)
+			},
+			wantCount: 0,
+		},
+		"Delete will remove only items with specified key": {
+			setup: func(t *testing.T, repo *ActiveUsersRepo) {
+				key1 := uuid.New()
+				key2 := uuid.New()
 
-	repo.Delete(key)
-	assert.Equal(t, 0, repo.Count())
+				require.NoError(t, repo.Add(key1))
+				require.NoError(t, repo.Add(key2))
+
+				repo.Delete(key1)
+			},
+			wantCount: 1,
+		},
+	}
+
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			repo := NewActiveUsersRepo()
+			c.setup(t, repo)
+
+			assert.Equal(t, c.wantCount, repo.Count())
+		})
+	}
 }
 
 func TestCount(t *testing.T) {
-	repo := NewActiveUsersRepo()
-	key := uuid.New()
+	cases := map[string]struct {
+		setup     func(t *testing.T, repo *ActiveUsersRepo)
+		wantCount int
+	}{
+		"Count without items": {
+			setup:     func(t *testing.T, repo *ActiveUsersRepo) {},
+			wantCount: 0,
+		},
+	}
 
-	require.NoError(t, repo.Add(key))
-	require.NoError(t, repo.Add(key))
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
 
-	assert.Equal(t, 1, repo.Count())
+			repo := NewActiveUsersRepo()
+			c.setup(t, repo)
+
+			assert.Equal(t, c.wantCount, repo.Count())
+		})
+	}
 }
